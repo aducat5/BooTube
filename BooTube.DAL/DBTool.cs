12781ee20f2afbdfcd95d2d4d22cbbb6a52c0f58
Tamer Berat Çelik <aducat5@hotmail.com>
2018-10-07 10:47:04 +0300

    Fırst upload of project files.

---
+++ BooTube.DAL/DBTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooTube.DAL
+{
+    public class DBTool
+    {
+        private DBTool() {}
+
+
+        private static BooTubeEntities _instance;
+
+        public static BooTubeEntities GetInstance()
+        {
+            if (_instance == null)
+                _instance = new BooTubeEntities();
+
+            return _instance;
+        }
+    }
+}
