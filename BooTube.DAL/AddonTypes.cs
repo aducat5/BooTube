b9a7a707427632660b8248efe5239a194a110c69
Tamer Berat Çelik <aducat5@hotmail.com>
2018-10-07 11:08:03 +0300

    DAL Layer Upload

---
+++ BooTube.DAL/AddonTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooTube.DAL
+{
+    public enum AddonTypes
+    {
+        Like = 2,
+        Dislike = 4,
+        Follow = 3,
+        Click = 5,
+        View = 6,
+        Share = 7
+    }
+}
